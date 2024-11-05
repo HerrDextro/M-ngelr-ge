61f3060d06fa76dbeea56fb69e038e1dba2f1eca
Neo <Neo@DESKTOP-OV7RN3A>
2024-11-05 10:40:26 +0100

    .

---
+++ UI.cs
@@ -0,0 +1,40 @@
+namespace Complaintform
+{
+    public class UI
+    {
+        static void run()
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+
+            Console.WriteLine("test"); //to see if repo is working like intended
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+
+
+        }
+    }
+}
